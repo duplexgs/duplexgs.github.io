8624fc97f85edf1ed205df5976edb70191512621
weihangliu <liuwh2023@shanghaitech.edu.cn>
2026-01-23 16:44:48 +0800

    add new windows

--- static/js/event_handler.js
+++ static/js/event_handler.js
@@ -88,6 +88,12 @@ document.addEventListener('DOMContentLoaded', domReady);
                     case 1:
                         image.src = './static/images/results/apartment2';
                         break;
+                    case 2:
+                        image.src = './static/images/results/kitchen1';
+                        break;
+                    case 3:
+                        image.src = './static/images/results/kitchen2';
+                        break;
                 }
                 switch (i) {
                     case 0:
@@ -106,6 +112,52 @@ document.addEventListener('DOMContentLoaded', domReady);
             }
 
             let scene_list = document.getElementById("apartment-scale-recon").children;
+            for (let i = 0; i < scene_list.length; i++) {
+                if (idx == i) {
+                    scene_list[i].children[0].className = "nav-link active";
+                } else {
+                    scene_list[i].children[0].className = "nav-link";
+                }
+            }
+        }
+
+        function additionalSceneEvent(idx) {
+            let dics = document.getElementById('additional-dics');
+            if (!dics) return;
+            
+            let sections = dics.getElementsByClassName('b-dics__section');
+            let imagesLength = 4;
+            
+            for (let i = 0; i < imagesLength; i++) {
+                let image = sections[i].getElementsByClassName('b-dics__image-container')[0].getElementsByClassName('b-dics__image')[0];
+                switch (idx) {
+                    case 0:
+                        image.src = './static/images/results/dj';
+                        break;
+                    case 1:
+                        image.src = './static/images/results/playroom';
+                        break;
+                    case 2:
+                        image.src = './static/images/results/matrixcity';
+                        break;
+                }
+                switch (i) {
+                    case 0:
+                        image.src = image.src + '_ours.png';
+                        break;
+                    case 1:
+                        image.src = image.src + '_SFGS.png';
+                        break;
+                    case 2:
+                        image.src = image.src + '_OctreeGS.png';
+                        break;
+                    case 3:
+                        image.src = image.src + '_ScaffoldGS.png';
+                        break;
+                }
+            }
+
+            let scene_list = document.getElementById("additional-scale-recon").children;
             for (let i = 0; i < scene_list.length; i++) {
                 if (idx == i) {
                     scene_list[i].children[0].className = "nav-link active";
